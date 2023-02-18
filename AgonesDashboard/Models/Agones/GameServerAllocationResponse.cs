bd1ec1a68ffb31e1c480ead7896da9f655d3ce1a
moepig <uncho.tnct@gmail.com>
2023-02-18 19:22:53 +0900

    https://agones.dev/site/docs/advanced/allocator-service/#using-rest に従い allocator のリクエスト/レスポンスを定義

---
+++ AgonesDashboard/Models/Agones/GameServerAllocationResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AgonesDashboard.Models
+{
+    public class GameServerAllocationResponse
+    {
+        [JsonPropertyName("gameServerName")]
+        string GameServerName { get; set; }
+
+        [JsonPropertyName("ports")]
+        IEnumerable<GameServerAllocationResponsePort> Ports { get; set; }
+
+        [JsonPropertyName("address")]
+        string Address { get; set; }
+
+        [JsonPropertyName("nodeName")]
+        string NodeName { get; set; }
+
+    }
+
+    public class GameServerAllocationResponsePort
+    {
+        [JsonPropertyName("name")]
+        string Name { get; set; }
+
+        [JsonPropertyName("port")]
+        string Port { get; set; }
+    }
+}
