00b4d22eda84f6dd6cc3bc9f7776473b4344b540
moepig <uncho.tnct@gmail.com>
2023-02-19 23:26:45 +0900

    Kubernetes API へのアクセスを行う予定の、中身が何もない Repository をとりあえず作成

---
+++ AgonesDashboard/Repositories/Kubernetes/GameServerRepository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AgonesDashboard.Repositories.Kubernetes
+{
+    public class GameServerRepository
+    {
+        private readonly ILogger<GameServerRepository> _logger;
+
+        public GameServerRepository(ILogger<GameServerRepository> logger)
+        {
+            _logger = logger;
+        }
+    } 
+}
