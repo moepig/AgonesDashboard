52ced67dbc98018924931f7066790bc621a94d46
moepig <uncho.tnct@gmail.com>
2023-02-25 18:52:08 +0900

    https://github.com/kubernetes-client/csharp/tree/master/examples/customResource を参考に見様見真似で書いてみる

---
+++ AgonesDashboard/Models/Kubernetes/CustomResource.cs
@@ -0,0 +1,37 @@
+﻿using k8s;
+using k8s.Models;
+using System.Text.Json.Serialization;
+
+
+namespace AgonesDashboard.Models.Kubernetes
+{
+    public class CustomResourceDefinition
+    {
+        public string Version { get; set; }
+        public string Group { get; set; }
+        public string PluralName { get; set; }
+        public string Kind { get; set; }
+        public string Namespace { get; set; }
+    }
+
+    public class CustomResource : KubernetesObject, IMetadata<V1ObjectMeta>
+    {
+        [JsonPropertyName("metadata")]
+        public V1ObjectMeta Metadata { get; set; }
+    }
+
+    public abstract class CustomResource<TSpec, TStatus> : CustomResource
+    {
+        [JsonPropertyName("spec")]
+        public TSpec Spec { get; set; }
+
+        [JsonPropertyName("CStatus")]
+        public TStatus CStatus { get; set; }
+    }
+
+    public class CustomResourceList<T> : KubernetesObject where T : CustomResource
+    {
+        public V1ListMeta Metadata { get; set; }
+        public List<T> Items { get; set; }
+    }
+}
---
+++ AgonesDashboard/Models/Kubernetes/CustomResources/GameServer.cs
@@ -0,0 +1,25 @@
+﻿using k8s;
+using k8s.Models;
+using System.Text.Json.Serialization;
+
+namespace AgonesDashboard.Models.Kubernetes.CustomResources
+{
+
+
+    public class GameServerResource : CustomResource<GameServerSpec, GameServerStatus>
+    {
+
+    }
+
+    public class GameServerSpec
+    {
+        [JsonPropertyName("cityName")]
+        public string CityName { get; set; }
+    }
+
+    public class GameServerStatus : V1Status
+    {
+        [JsonPropertyName("temperature")]
+        public string Temperature { get; set; }
+    }
+}
