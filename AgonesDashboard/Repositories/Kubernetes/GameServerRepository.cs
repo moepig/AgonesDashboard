1a47879b37309f271f05d5fc5037c155b8842c5a
moepig <fujita.riki@gmail.com>
2023-02-25 18:52:08 +0900

    https://github.com/kubernetes-client/csharp/tree/master/examples/customResource を参考に見様見真似で書いてみる

--- AgonesDashboard/Repositories/Kubernetes/GameServerRepository.cs
+++ AgonesDashboard/Repositories/Kubernetes/GameServerRepository.cs
@@ -1,13 +1,40 @@
-﻿
+﻿using AgonesDashboard.Models.Kubernetes;
+using AgonesDashboard.Models.Kubernetes.CustomResources;
+using k8s;
+
 namespace AgonesDashboard.Repositories.Kubernetes
 {
     public class GameServerRepository
     {
         private readonly ILogger<GameServerRepository> _logger;
 
+        private GenericClient _client;
+
         public GameServerRepository(ILogger<GameServerRepository> logger)
         {
             _logger = logger;
+        }
+
+        public async Task<CustomResourceList<GameServerResource>> ListAsync()
+        {
+            var client = GetClient();
+            var result = await client.ListAsync<CustomResourceList<GameServerResource>>().ConfigureAwait(false);
+
+            return result;
+        }
+
+        private GenericClient GetClient()
+        {
+            if (_client is not null)
+            {
+                return _client;
+            }
+
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var kubernetes = new k8s.Kubernetes(config);
+            _client = new GenericClient(kubernetes, "agones.dev", "v1", "gameservers");
+
+            return _client;
         }
     } 
 }
