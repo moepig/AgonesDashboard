4861a334793f193c759b864aac0cdc79bb5d1ac4
moepig <uncho.tnct@gmail.com>
2023-02-26 21:29:14 +0900

    Fleet に対応

---
+++ AgonesDashboard/Models/Kubernetes/CustomResources/Agones/V1AggregatedPlayerStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AgonesDashboard.Models.Kubernetes.CustomResources.Agones
+{
+    // https://agones.dev/site/docs/reference/agones_crd_api_reference/#agones.dev/v1.AggregatedPlayerStatus
+    public class V1AggregatedPlayerStatus
+    {
+        [JsonPropertyName("count")]
+        public long Count { get; set; }
+
+        [JsonPropertyName("capacity")]
+        public long Capacity { get; set; }
+    }
+}
