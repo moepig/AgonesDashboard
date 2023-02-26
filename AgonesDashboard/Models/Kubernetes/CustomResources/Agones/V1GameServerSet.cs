1d2dec8cfe531f7aa74f421470f0d5498f3d2607
moepig <fujita.riki@gmail.com>
2023-02-26 22:26:16 +0900

    GameServerSet を追加

---
+++ AgonesDashboard/Models/Kubernetes/CustomResources/Agones/V1GameServerSet.cs
@@ -0,0 +1,43 @@
+﻿using k8s.Models;
+using System.Text.Json.Serialization;
+
+namespace AgonesDashboard.Models.Kubernetes.CustomResources.Agones
+{
+    public class V1GameServerSet : CustomResource<V1GameServerSetSpec, V1GameServerSetStatus>
+    {
+    }
+
+    public class V1GameServerSetSpec
+    {
+        [JsonPropertyName("replicas")]
+        public int Replicas { get; set; }
+
+        // agones.dev/agones/pkg/apis.SchedulingStrategy	
+        [JsonPropertyName("scheduling")]
+        public string? Scheduling { get; set; }
+
+        [JsonPropertyName("template")]
+        public V1GameServerTemplateSpec? Template { get; set; }
+    }
+
+    public class V1GameServerSetStatus : V1Status
+    {
+        [JsonPropertyName("replicas")]
+        public int Replicas { get; set; }
+
+        [JsonPropertyName("readyReplicas")]
+        public int ReadyReplicas { get; set; }
+
+        [JsonPropertyName("reservedReplicas")]
+        public int ReservedReplicas { get; set; }
+
+        [JsonPropertyName("allocatedReplicas")]
+        public int AllocatedReplicas { get; set; }
+
+        [JsonPropertyName("shutdownReplicas")]
+        public int ShutdownReplicas { get; set; }
+
+        [JsonPropertyName("players")]
+        public V1AggregatedPlayerStatus? Players { get; set; }
+    }
+}
