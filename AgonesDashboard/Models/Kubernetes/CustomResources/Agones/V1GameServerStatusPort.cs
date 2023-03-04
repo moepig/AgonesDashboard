7c88661fa09d2bfa841643488d3ce288f764de62
moepig <fujita.riki@gmail.com>
2023-03-04 14:43:34 +0900

    allocation.agones.dev/v1.GameServerAllocation の定義を作成

---
+++ AgonesDashboard/Models/Kubernetes/CustomResources/Agones/V1GameServerStatusPort.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AgonesDashboard.Models.Kubernetes.CustomResources.Agones
+{
+    // https://agones.dev/site/docs/reference/agones_crd_api_reference/#agones.dev/v1.GameServerStatusPort
+    public class V1GameServerStatusPort
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("port")]
+        public int Port { get; set; }
+    }
+}
