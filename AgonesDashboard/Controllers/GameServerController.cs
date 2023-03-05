b5f8a3a3b75b700caf1fb35ac9ca64ab566e3ed4
moepig <uncho.tnct@gmail.com>
2023-03-05 18:22:28 +0900

    View の見た目確認用にモックを返す Controller メソッドを作成

--- AgonesDashboard/Controllers/GameServerController.cs
+++ AgonesDashboard/Controllers/GameServerController.cs
@@ -1,6 +1,7 @@
 ﻿using AgonesDashboard.Repositories;
 using AgonesDashboard.Repositories.Kubernetes;
 using AgonesDashboard.Services;
+using AgonesDashboard.ViewModels.GameServer;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
@@ -35,6 +36,58 @@ namespace AgonesDashboard.Controllers
 
             return View(viewModel);
         }
+
+        public ViewResult IndexViewTest()
+        {
+            var gameServer1 = new GameServerSimple
+            {
+                Name = "gameServer1",
+                ContainerPort = 8080,
+                HostPort = 80,
+                Protocol = "TCP",
+                GameServerSimpleContainer = new List<GameServerSimpleContainer>
+                {
+                    new GameServerSimpleContainer { Name = "container1", Image = "image1" },
+                    new GameServerSimpleContainer { Name = "container2", Image = "image2" },
+                    new GameServerSimpleContainer { Name = "container3", Image = "image3" }
+                }
+            };
+
+            var gameServer2 = new GameServerSimple
+            {
+                Name = "gameServer2",
+                ContainerPort = 9090,
+                HostPort = 90,
+                Protocol = "UDP",
+                GameServerSimpleContainer = new List<GameServerSimpleContainer>
+                {
+                    new GameServerSimpleContainer { Name = "container3", Image = "image3" },
+                }
+            };
+
+            var gameServers = new Dictionary<string, IList<GameServerSimple>>
+            {
+                { "namespace1", new List<GameServerSimple> { gameServer1 } },
+                { "namespace2", new List<GameServerSimple> { gameServer2 } }
+            };
+
+            var containerTotal = new Dictionary<string, int>();
+
+            foreach (var (k, v) in gameServers)
+            {
+                var count = v.SelectMany(x => x.GameServerSimpleContainer).Count();
+                containerTotal.Add(k, count);
+            }
+
+            var viewModel = new ViewModels.GameServer.Index
+            {
+                GameServers = (IDictionary<string, IList<GameServerSimple>>)gameServers,
+                ContainerTotal = containerTotal,
+            };
+
+            return View("Index", viewModel);
+        }
+        
         public async Task<string> Test()
         {
             var list = await _gameServerRepository.ListAsync();
