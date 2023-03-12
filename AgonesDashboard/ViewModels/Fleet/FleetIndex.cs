c3e7ef68f2959187e59ade9d069d6dc20b491a45
moepig <fujita.riki@gmail.com>
2023-03-12 16:58:53 +0900

    Fleet の表示を実装

---
+++ AgonesDashboard/ViewModels/Fleet/FleetIndex.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace AgonesDashboard.ViewModels.Fleet
+{
+    public class FleetIndex : AbstractViewModel
+    {
+        // key: namespace
+        public IDictionary<string, IList<FleetSimple>> Fleets { get; set; }
+    }
+    public class FleetSimple
+    {
+        public string? Name { get; set; }
+        public string? GameServerName { get; set; }
+        public string? Scheduling { get; set; }
+        public int? ReadyReplicas { get; set; }
+        public int? ReservedReplicas { get; set; }
+        public int? AllocatedReplicas { get; set; }
+    }
+}
