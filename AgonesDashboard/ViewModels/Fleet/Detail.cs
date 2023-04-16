4bd93c6655a299a4fe728f724daf97153991cb03
moepig <uncho.tnct@gmail.com>
2023-04-16 19:03:05 +0900

    Fleet の詳細に FleetAutoscaler の情報もセットで出す

--- AgonesDashboard/ViewModels/Fleet/Detail.cs
+++ AgonesDashboard/ViewModels/Fleet/Detail.cs
@@ -1,9 +1,11 @@
 ﻿using AgonesDashboard.Models.Kubernetes.CustomResources.Agones;
+using AgonesDashboard.Models.Kubernetes.CustomResources.Agones.AutoScaling;
 
 namespace AgonesDashboard.ViewModels.Fleet
 {
     public class Detail : AbstractViewModel
     {
         public required V1Fleet Fleet { get; init; }
+        public required V1FleetAutoscaler? FleetAutoscaler { get; init; }
     }
 }
